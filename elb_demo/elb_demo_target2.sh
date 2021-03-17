9fd686f165b0c970a647fa4b915a7a0383b77fb6
Atamankina <gatamank@147dda0b7cd4.ant.amazon.com>
2021-03-17 20:31:09 +0100

    Fix ELB target demo paths.

--- elb_demo/elb_demo_target2.sh
+++ elb_demo/elb_demo_target2.sh
@@ -5,7 +5,7 @@ yum install -y httpd
 wget https://raw.githubusercontent.com/atamankina/demos/main/elb_demo/elb_demo_target2.html
 sed -e "s/\${host}/$(hostname -f)/" \
 	-e "s/\${AZ}/$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)/" \
-	elb_demo.html > /var/www/html/index.html
+	elb_demo.html > /var/www/html/target2.html
 # Turn on web server
 chkconfig httpd on
 service httpd start
