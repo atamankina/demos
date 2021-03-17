c261ef9e155f596e2c040acb3dde10af66cc864a
Atamankina <gatamank@147dda0b7cd4.ant.amazon.com>
2021-03-17 20:28:58 +0100

    Fix ELB target demo URLs.

--- elb_demo/elb_demo_target2.sh
+++ elb_demo/elb_demo_target2.sh
@@ -2,7 +2,7 @@
 # Install Apache Web Server and PHP
 yum install -y httpd
 # Download and adjust the html file
-wget https://raw.githubusercontent.com/atamankina/demos/elb_demo/main/elb_demo_target2.html
+wget https://raw.githubusercontent.com/atamankina/demos/main/elb_demo/elb_demo_target2.html
 sed -e "s/\${host}/$(hostname -f)/" \
 	-e "s/\${AZ}/$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)/" \
 	elb_demo.html > /var/www/html/index.html
