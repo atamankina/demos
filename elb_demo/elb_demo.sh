b63e5f5363e946bf51960f79eb67b94ca1a2b1bd
Galina Atamankina <g.atamankina@gmail.com>
2024-01-24 12:11:36 +0100

    Update elb_demo.sh - fix the comment, add separation lines

--- elb_demo/elb_demo.sh
+++ elb_demo/elb_demo.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# Install Apache Web Server and PHP
+# Install Apache Web Server
 yum update -y
 yum install -y httpd
+
 # Download and adjust the html file
 wget https://raw.githubusercontent.com/atamankina/demos/main/elb_demo/elb_demo.html
 sed -e "s/\${host}/$(hostname -f)/" \
 	-e "s/\${AZ}/$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)/" \
 	elb_demo.html > /var/www/html/index.html
+
 # Turn on web server
 chkconfig httpd on
 service httpd start
